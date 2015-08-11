e4aa449615985c74b3f22c55afe4b917f39a03ae
Will Oursler <woursler@Wills-MacBook-Pro.local>
2015-08-11 14:37:15 -0700

    Adds a comment accrued while working on docs

--- orphanblack/api.py
+++ orphanblack/api.py
@@ -2,3 +2,5 @@
 # Run a scan on a particular path with any file selection criteria
 # Load results of any previous scan
 # Automatically find canidate results to load.
+
+# TODO: Do apis take filenames or file objects?
